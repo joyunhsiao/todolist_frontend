fd4a1303bf0c0289c0a06337b231b0c42650d7d3
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-06-17 23:08:43 +0800

    chore: Change port from 5173 to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react-todolist' : '/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    base: process.env.NODE_ENV === 'production' ? '/react-todolist' : '/',
+    server: {
+      port: parseInt(env.VITE_FRONTEND_PORT),
+      host: true
+    },
+    plugins: [react()]
+  }
 })
