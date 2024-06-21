216337af9e046951ad53fa053ee1c8990f10a1c0
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-06-21 10:58:24 +0800

    feat: Consolidate variable naming conventions

--- src/routes/AuthLayout.tsx
+++ src/routes/AuthLayout.tsx
@@ -4,7 +4,7 @@ export const AuthLayout: React.FC = () => {
 
   return (
     <div className='auth_container'>
-      <h1 className='visually-hidden'>TodoList</h1>
+      <h1 className='visually_hidden'>TodoList</h1>
       <img width='386px' src='/images/left.svg' alt='online todo list' />
       <div className='auth_right'>
         <Outlet />
