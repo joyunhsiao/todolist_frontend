a48d0924ff15d06e88347348869be11d72c3a991
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-06-21 10:58:48 +0800

    feat: Add todolist empty UI

--- src/routes/TodoList.tsx
+++ src/routes/TodoList.tsx
@@ -1,9 +1,31 @@
+import { Navigate } from 'react-router-dom'
+
 export const TodoList: React.FC = () => {
+  const hasAuth = true
+  if (!hasAuth) return <Navigate to='log_in' replace={true} />
 
   return (
     <>
-      <h1 className='visually-hidden'>TodoList</h1>
-      <h2>TodoList</h2>
+      <div className='todolist_container'>
+        <h1 className='visually_hidden'>TodoList</h1>
+        <header className='todolist_header'>
+          <img src='/images/logo.svg' alt='online todo list' />
+          <div className='todolist_headerRight'>
+            <p>王小明的待辦</p>
+            <a href='/log_in'>登出</a>
+          </div>
+        </header>
+        <main>
+          <div className='addTodo_box'>
+            <input type='text' name='addTodo_box_input' id='addTodo_input' className='addTodo_input' placeholder='新增待辦事項'/>
+            <button name='addTodo_button' type='submit' className='addTodo_button'>
+              <img src='/images/plus.svg' alt='Add todo' />
+            </button>
+          </div>
+          <p className='noTodo_text'>目前尚無待辦事項</p>
+          <img className='noTodo_img' src='/images/empty.svg' alt='There is currently no to-do list' />
+        </main>
+      </div>
     </>
   )
 }
